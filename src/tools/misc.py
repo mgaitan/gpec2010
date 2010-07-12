b11129544752df87759c46675f7d132dac4d6fa2
gaitan <gaitan@5a9e655f-be75-9245-2be8-98025327a8b8>
2010-07-12 21:45:52 +0000

    curry

---
+++ src/tools/misc.py
@@ -0,0 +1,16 @@
+class curry:
+    """Taken from the Python Cookbook, this class provides an easy way to
+    tie up a function with some default parameters and call it later.
+    See http://aspn.activestate.com/ASPN/Cookbook/Python/Recipe/52549 for more.
+    """
+    def __init__(self, func, *args, **kwargs):
+        self.func = func
+        self.pending = args[:]
+        self.kwargs = kwargs
+    def __call__(self, *args, **kwargs):
+        if kwargs and self.kwargs:
+            kw = self.kwargs.copy()
+            kw.update(kwargs)
+        else:
+            kw = kwargs or self.kwargs
+        return self.func(*(self.pending + args), **kw)
