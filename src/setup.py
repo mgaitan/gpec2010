0f4f8b0022a9acc74d2a7186f9a1822b86e36521
gaitan <gaitan@5a9e655f-be75-9245-2be8-98025327a8b8>
2010-09-29 01:09:54 +0000

    setup.py file for py2exe compilation

---
+++ src/setup.py
@@ -0,0 +1,33 @@
+from distutils.core import setup
+import py2exe
+import matplotlib
+ 
+data = matplotlib.get_py2exe_datafiles() 
+
+includes = ['numpy.core.umath']
+excludes = ['_gtkagg', '_tkagg', 'bsddb', 'curses', 'email', 'pywin.debugger',
+            'pywin.debugger.dbgcon', 'pywin.dialogs', 'tcl',
+            'Tkconstants', 'Tkinter']
+packages = []
+dll_excludes = ['libgdk-win32-2.0-0.dll', 'libgobject-2.0-0.dll', 'tcl84.dll',
+                'tk84.dll']
+  
+
+setup(
+    data_files = data, 
+    options = {"py2exe": {"compressed": 2,
+                          "optimize": 2,
+                          "includes": includes,
+                          "excludes": excludes,
+                          "packages": packages,
+                          "dll_excludes": dll_excludes,
+                          "bundle_files": 3,
+                          "dist_dir": "dist",
+                          "xref": False,
+                          "skip_archive": False,
+                          "ascii": False,
+                          "custom_boot_script": '',
+                         }
+              },
+    windows=['aui.py']
+)
